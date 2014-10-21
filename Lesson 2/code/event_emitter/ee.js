6458844c104a850ee83287a332bd0c94e56bbbd0
zireael007 <zoe7.dk@gmail.com>
2014-10-21 22:59:36 +0200

    zadanie 2

--- Lesson 2/code/event_emitter/ee.js
+++ Lesson 2/code/event_emitter/ee.js
@@ -11,10 +11,31 @@
 	};
 
 	EE.prototype.on = function (eventName, listener, context) {
-
+			if (typeof(this.listeners[eventName]) === "undefined") {
+				this.listeners[eventName] = [];
+			}
+			
+			var temp = listener.bind(context);
+			this.listeners[eventName].push(temp);
+			var tempListeners = this.listeners;
+			
+			return function () {
+				var index = tempListeners[eventName].indexOf(temp);
+				if (index >= 0)
+					{ tempListeners[eventName].splice(index, 1); }
+				};
+			
 	};
 
 	EE.prototype.emit = function (eventName /*, other args...*/) {
+			
+			var args = [];
+			Array.prototype.push.apply(args, arguments);
+			args.splice(0,1); 
+			
+			for(i in this.listeners[eventName]) {
+            this.listeners[eventName][i].apply(this, args);
+			}
 
 	};
 
